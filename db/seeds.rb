ec386c54f024b87755997a869835a414e97f6c76
Jacob <jakegibs617@gmail.com>
2015-10-05 13:18:32 -0400

    initial

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,14 @@ require 'faker'
 User.create(email: "admin@email.com",
             encrypted_password: "$2a$10$8IJ8lAADmoL0yU6no0BaJOH2P2hSkmcS4",
             sign_in_count: 1,
-						created_at: "2015-10-05 15:14:54.746525",
-						updated_at: "2015-10-05 16:15:54.751009",
+            created_at: "2015-10-05 15:14:54.746525",
+            updated_at: "2015-10-05 16:15:54.751009",
             username: "admin",
-						role: "admin")
+            role: "admin")
 User.create(email: "member@email.com",
             encrypted_password: "$2a$10$8IJ8lAADmoL0yU6no0BaJOH2P2hSkmS4s",
             sign_in_count: 1,
-						created_at: "2015-10-05 15:16:54.746525",
-						updated_at: "2015-10-05 16:15:56.751509",
+            created_at: "2015-10-05 15:16:54.746525",
+            updated_at: "2015-10-05 16:15:56.751509",
             username: "member",
-						role: "member")
+            role: "member")
