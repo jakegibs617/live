f4bd376ebaa381dce6326aa9a9900ac68f664f39
Jacob <jakegibs617@gmail.com>
2015-10-05 12:53:52 -0400

    initial

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable
+
+  mount_uploader :profile_photo, ProfilePhotoUploader
 
   def admin?
    role == "admin"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+User.create(email: "admin@email.com",
+            encrypted_password: "$2a$10$8IJ8lAADmoL0yU6no0BaJOH2P2hSkmcS4",
+            sign_in_count: 1,
+						created_at: "2015-10-05 15:14:54.746525",
+						updated_at: "2015-10-05 16:15:54.751009",
+            username: "admin",
+						role: "admin")
+User.create(email: "member@email.com",
+            encrypted_password: "$2a$10$8IJ8lAADmoL0yU6no0BaJOH2P2hSkmS4s",
+            sign_in_count: 1,
+						created_at: "2015-10-05 15:16:54.746525",
+						updated_at: "2015-10-05 16:15:56.751509",
+            username: "member",
+						role: "member")
