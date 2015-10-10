dfca1527069d92f4bce8c5f6c9157f04eaf5eef2
Jacob <jakegibs617@gmail.com>
2015-10-10 08:10:39 -0400

    fixed comments test

--- spec/features/user_creates_comment_spec.rb
+++ spec/features/user_creates_comment_spec.rb
@@ -6,12 +6,12 @@ feature 'user creates comment', %{
   So that other users can see what I think of the event
 } do
   scenario 'creates comment succcessfully' do
-		user1 = FactoryGirl.create(:user)
+    user1 = FactoryGirl.create(:user)
     user2 = FactoryGirl.create(:user)
     event = FactoryGirl.create(:event, owner: user1)
 
-		sign_in(user2)
-		click_on event.title
+    sign_in(user2)
+    click_on event.title
 
     click_link "Add Comment"
     choose 3
@@ -20,21 +20,20 @@ feature 'user creates comment', %{
 
     expect(page).to have_content("Comment added.")
     expect(page).to have_content("I'll be there!")
-
   end
 
   scenario 'create unsucccessful comment, without rating' do
-		user1 = FactoryGirl.create(:user)
-		user2 = FactoryGirl.create(:user)
-		event = FactoryGirl.create(:event, owner: user1)
+    user1 = FactoryGirl.create(:user)
+    user2 = FactoryGirl.create(:user)
+    event = FactoryGirl.create(:event, owner: user1)
 
-		sign_in(user2)
+    sign_in(user2)
 
-		click_on event.title
-		click_link "Add Comment"
+    click_on event.title
+    click_link "Add Comment"
     fill_in "Body", with: "Test"
 
-		click_on "Create Comment"
+    click_on "Create Comment"
     expect(page).to have_content("Rating can't be blank")
   end
 end
