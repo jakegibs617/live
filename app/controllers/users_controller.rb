f5c31223d1196b4e86cd89be2d9193521d467295
Jacob <jakegibs617@gmail.com>
2015-10-10 08:08:04 -0400

    fixed comments test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,11 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = current_user
-  end
-
-  def new
-    @user = User.new
-  end
+  # def show
+  #   @user = current_user
+  # end
+  #
+  # def new
+  #   @user = User.new
+  # end
 
   def index
     @admins = User.where(role: "admin")
--- spec/features/user_creates_comment_spec.rb
+++ spec/features/user_creates_comment_spec.rb
@@ -1,70 +1,40 @@
-# Failure/Error: click_on 'Bar'
-#     ActionView::Template::Error:
-#       undefined method `username' for nil:NilClass
+require 'rails_helper'
 
-# require 'rails_helper'
-#
-# feature 'user creates comment', %{
-#   As a signed up user
-#   I want to create new comment
-#   So that other users can see what I think of the event
-# } do
-#   scenario 'creates comment succcessfully' do
-#     event = FactoryGirl.create(:event)
-#     user = FactoryGirl.create(:user)
-#
-#     sign_in(user)
-#     visit root_path
-#
-#     click_on 'Bar'
-#
-#
-#     click_on 'Add Comment'
-#
-#     choose 3
-#
-#     fill_in "Body", with: "I'll be there!"
-#
-#     click_on "Create Comment"
-#
-#     expect(page).to have_content("Comment added.")
-#     expect(page).to have_content("I'll be there!")
-#
-#   end
-#
-#   scenario 'create unsucccessful comment, without rating' do
-#     event = FactoryGirl.create(:event)
-#     user = FactoryGirl.create(:user)
-#
-#     sign_in(event.user)
-#
-#     click_on event.title
-#
-#     click_on 'Add Comment'
-#
-#     fill_in "Body", with: "Test"
-#
-#     click_on "Create Comment"
-#
-#     expect(page).to have_content("Rating can't be blank")
-#   end
-#
-#   scenario 'create unsucccessful comment, without description' do
-#     event = FactoryGirl.create(:event)
-#     user = FactoryGirl.create(:user)
-#
-#     sign_in(event.user)
-#
-#     click_on event.title
-#
-#     click_on 'Add Comment'
-#
-#     choose 3
-#
-#     fill_in "Body", with: ""
-#
-#     click_on "Create Comment"
-#
-#     expect(page).to have_content("Body can't be blank")
-#   end
-# end
+feature 'user creates comment', %{
+  As a signed up user
+  I want to create new comment
+  So that other users can see what I think of the event
+} do
+  scenario 'creates comment succcessfully' do
+		user1 = FactoryGirl.create(:user)
+    user2 = FactoryGirl.create(:user)
+    event = FactoryGirl.create(:event, owner: user1)
+
+		sign_in(user2)
+		click_on event.title
+
+    click_link "Add Comment"
+    choose 3
+    fill_in "Body", with: "I'll be there!"
+    click_on "Create Comment"
+
+    expect(page).to have_content("Comment added.")
+    expect(page).to have_content("I'll be there!")
+
+  end
+
+  scenario 'create unsucccessful comment, without rating' do
+		user1 = FactoryGirl.create(:user)
+		user2 = FactoryGirl.create(:user)
+		event = FactoryGirl.create(:event, owner: user1)
+
+		sign_in(user2)
+
+		click_on event.title
+		click_link "Add Comment"
+    fill_in "Body", with: "Test"
+
+		click_on "Create Comment"
+    expect(page).to have_content("Rating can't be blank")
+  end
+end
