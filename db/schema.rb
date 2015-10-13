7f13e048a52c15156e3d7fe2e8ea959fa4215b38
Jacob <jakegibs617@gmail.com>
2015-10-13 15:35:55 -0400

    hound

--- db/schema.rb
+++ db/schema.rb
@@ -51,12 +51,6 @@ ActiveRecord::Schema.define(version: 20151013144312) do
     t.datetime "updated_at"
   end
 
-  create_table "locations", force: :cascade do |t|
-    t.string "address"
-    t.float  "latitude"
-    t.float  "longitude"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",       null: false
     t.string   "encrypted_password",     default: "",       null: false
