21b3fa13eefc3a871c3d900a7e410d28a7dea3b6
Jacob <jakegibs617@gmail.com>
2015-10-14 17:08:56 -0400

    user-profile-badges

--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -5,13 +5,13 @@ end
 badges = [
   {id: 1,
    name: "party starter badge",
-   custom_fields: { image: "/event_1.png" }},
+   custom_fields: { image: "/event_1.png" } },
   { id: 2, name: 'Novice Commenter',
-    custom_fields: { image: "/comment_1.png" }},
+    custom_fields: { image: "/comment_1.png" } },
   { id: 3, name: 'Novice Rater',
-    custom_fields: { image: "/rating_1.png" }},
+    custom_fields: { image: "/rating_1.png" } },
   { id: 4, name: 'Hello popular',
-    custom_fields: { image: "/add_friend_1.png" }}
+    custom_fields: { image: "/add_friend_1.png" } }
 ]
 
 badges.each do |badge|
